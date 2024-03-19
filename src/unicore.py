457f2a24c75faca87e98ddd413b288ca4f67741c
DW Kim <endix1029@gmail.com>
2024-03-19 17:21:20 +0900

    redirect module names

--- src/unicore.py
+++ src/unicore.py
@@ -2,22 +2,9 @@ from sys import argv, exit
 
 from envs import variables as var
 from envs import error_handler as err
-import modules.version
+from modules import version
 
-VALID_MODULES = {
-    "version": modules.version.run
-}
-def init():
-    var.config_init(f'{var.PARENT_DIR}/path.cfg')
-    if len(argv) < 2:
-        usage()
-        exit(0)
-    if not argv[1] in VALID_MODULES:
-        err.warning(err.ERR_MODULE_NOT_FOUND, argv[1])
-        usage()
-        exit(1)
-
-def usage():
+def usage(_):
     print(f"Unicore v{var.VERSION} {var.STABLE_TEXT}")
     print("Usage: unicore <module> [args]")
     print("")
@@ -25,6 +12,24 @@ def usage():
     print("  version     - Print version and information")
     print("  more to come...")
     print("")
+
+VALID_MODULES = {
+    "help": usage,
+    "usage": usage,
+    "info": version.run,
+    "version": version.run
+}
+def init():
+    var.config_init(f'{var.PARENT_DIR}/path.cfg')
+    if len(argv) < 2:
+        usage(None)
+        exit(0)
+    if not argv[1] in VALID_MODULES:
+        err.warning(err.ERR_MODULE_NOT_FOUND, argv[1])
+        usage(None)
+        exit(1)
+
+
 
 def run():
     VALID_MODULES[argv[1]](argv[2:])
