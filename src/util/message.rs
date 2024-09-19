9eef5106905f9bad0f4da32103ab38efb5f8fb39
Daniel DW Kim <endix1029@gmail.com>
2024-09-19 15:39:48 +0900

    flush after printing without line feed

--- src/util/message.rs
+++ src/util/message.rs
@@ -1,7 +1,13 @@
+use std::io::Write;
 use crate::envs::variables::verbosity as system_verbosity;
+use crate::envs::error_handler as err;
 pub fn print_message(msg: &String, verbosity: u8) {
     if verbosity <= system_verbosity() {
         print!("{}", msg);
+        match std::io::stdout().flush() {
+            Ok(_) => {},
+            Err(e) => err::error(err::ERR_GENERAL, Some(e.to_string())),
+        }
     }
 }
 pub fn println_message(msg: &String, verbosity: u8) {
