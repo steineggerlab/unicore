2ab05334dc8bc3f7753c36aeb35c28d5ac0680b8
pskvins <pskvins@snu.ac.kr>
2026-03-11 23:48:22 +0900

    Add option for choosing aa/3di/aa+3di MSA. Also generates partition file according to it. Still need to work on the tree inference command

--- src/seq/combine_fasta.rs
+++ src/seq/combine_fasta.rs
@@ -1,5 +1,6 @@
 use std::fs::File;
 use std::path::Path;
+use std::collections::HashMap;
 use std::io::{self, BufRead, BufReader, BufWriter, Write};
 use crate::envs::error_handler as err;
 
@@ -24,7 +25,7 @@ fn skip_to_next_fasta_line(reader: &mut BufReader<File>, sequence: &mut String, 
     }
     Ok(())
 }
-pub fn combine_fasta(fasta_files: &Vec<String>, output: &String) -> Result<(), Box<dyn std::error::Error>> {
+pub fn combine_fasta(fasta_files: &Vec<String>, output: &String, msa_for_tree: &u8) -> Result<(), Box<dyn std::error::Error>> {
     let mut names: Vec<String> = Vec::new();
     let mut sequences: Vec<String> = Vec::new();
     let mut prev_len = 0;
@@ -33,6 +34,8 @@ pub fn combine_fasta(fasta_files: &Vec<String>, output: &String) -> Result<(), B
     let partition_file = Path::new(&output).join("combined.fasta.partitions");
     
     let mut partition = BufWriter::new(File::create(partition_file)?);
+    let mut processed_fasta_files = HashMap::new();
+    let mut di = false;
     // Process each FASTA file
     for fasta_path in fasta_files {
         let file = File::open(fasta_path.trim())?;
@@ -42,6 +45,11 @@ pub fn combine_fasta(fasta_files: &Vec<String>, output: &String) -> Result<(), B
             .and_then(|p| p.file_name())
             .and_then(|name| name.to_str())
             .unwrap_or("unknown");
+        if processed_fasta_files.contains_key(hash) {
+            di = true;
+        } else {
+            processed_fasta_files.insert(hash, true);
+        }
         let mut reader = BufReader::new(file);
         let mut line = String::new();
         let mut add_this = 0;
@@ -90,7 +98,11 @@ pub fn combine_fasta(fasta_files: &Vec<String>, output: &String) -> Result<(), B
         }
 
         // Write to partition file
-        writeln!(partition, "JTT+F+I+G, {}={}-{}", hash, prev_len + 1, prev_len + add_this)?;
+        if di || *msa_for_tree == 1 {
+            writeln!(partition, "GH_AF_3DI+F+I+G, {}={}-{}", hash, prev_len + 1, prev_len + add_this)?;
+        } else {
+            writeln!(partition, "JTT+F+I+G, {}={}-{}", hash, prev_len + 1, prev_len + add_this)?;
+        }
 
         prev_len += add_this;
         // Pad sequences if shorter than prev_len
